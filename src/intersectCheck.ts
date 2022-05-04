de672d78c365be0977fba867bf4bf9127702b646
Vegar Ringdal <vegar.ringdal@gmail.com>
2022-05-04 22:59:17 +0200

    add size result when intersect

--- src/intersectCheck.ts
+++ src/intersectCheck.ts
@@ -1,3 +1,5 @@
+import { getBoundingBoxSizes } from "./getBoundingBoxSizes.ts";
+
 type BoundingBoxMaxMin = {
   x: number;
   y: number;
@@ -19,5 +21,21 @@ export function intersectCheck(a: bb, b: bb) {
     (a.max.z >= b.max.z && a.min.z <= b.max.z) ||
     (a.min.z <= b.min.z && a.max.z > b.min.z);
 
-  return { x, y, z };
+  const sizeA = getBoundingBoxSizes(a.max, a.min);
+  const sizeB = getBoundingBoxSizes(b.max, b.min);
+  const result = { x: 0, y: 0, z: 0, size: 0 };
+  if (x) {
+    result.x = Math.abs(sizeA.x - sizeB.x);
+    result.size = result.size + result.x;
+  }
+  if (y) {
+    result.y = Math.abs(sizeA.y - sizeB.y);
+    result.size = result.size + result.y;
+  }
+  if (z) {
+    result.z = Math.abs(sizeA.z - sizeB.z);
+    result.size = result.size + result.z;
+  }
+
+  return result;
 }
--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ console.log(result);
 
 const bbox1 = generateBoundingBox([3, 3, 3, 1, 1, 1]);
 
-const bbox2 = generateBoundingBox([5, 5, 5, 3, 3, 3]);
+const bbox2 = generateBoundingBox([5, 5, 5, 2, 2, 2]);
 console.log(bbox1);
 console.log(bbox2);
 console.log(intersectCheck(bbox1, bbox2));
